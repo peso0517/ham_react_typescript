e176f2222c58e654447792d5bb3a962d40967560
peso0517 <peso0517@gmail.com>
2022-06-12 21:48:52 +0900

    react components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { isMemberName } from "typescript";
 
-function App() {
+interface AppProps {
+  message?: string;
+  isMessage?: boolean;
+}
+
+const App: React.FunctionComponent<AppProps> = (props) => {
+  // const { message, contact } = props;
+  // const App = (props: AppProps) => {
+  console.log(props.isMessage);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{props.message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LET's Learn React
         </a>
       </header>
     </div>
   );
-}
+};
+
+// messageがない場合、defaultが表示されるようになる
+// functionに対して、型制約を実行しないと、デフォルト値がAppPropsと違っても通してしまうので注意!!
+App.defaultProps = {
+  message: "default",
+  // description: "This is App components",
+};
 
 export default App;
