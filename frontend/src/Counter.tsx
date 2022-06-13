4eb62a2a57ea917141c1c56499aa0070b3e9a99c
peso0517 <peso0517@gmail.com>
2022-06-13 18:39:31 +0900

    useRef

--- frontend/src/Counter.tsx
+++ frontend/src/Counter.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { isPlusToken } from "typescript";
 
 const Counter: React.FC<{}> = () => {
   const initialCount: any = 0;
   const [count, setCount] = useState<number>(initialCount);
   const plus = () => {
-    setCount((prevState) => prevState++);
+    setCount((prevState) => prevState + 1);
   };
   const minus = () => {
-    setCount((prevState) => prevState--);
+    setCount((prevState) => prevState - 1);
   };
+
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <div>
       <div>value: {count}</div>
       <button onClick={plus}>+1</button>
       <button onClick={minus}>-1</button>
+      <div>This components was re-rendered {renderTimes.current}</div>
     </div>
   );
 };
